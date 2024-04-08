d27f70e957f9b12bb802e0d677860800261f198a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-08 14:29:38 -0300

    Create conversao_de_tipos.py

---
+++ conversao_de_tipos.py
@@ -0,0 +1,25 @@
+#inteiro para float
+
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10/2
+print(preco)
+
+#float para inteiro
+preca = 10.30
+print(preca)
+
+preca = int(preca)
+print(preca)
+
+#conversão para divisão
+
+dindin = 10
+print(dindin)
+
+print(dindin / 2)
+print(dindin // 2)
