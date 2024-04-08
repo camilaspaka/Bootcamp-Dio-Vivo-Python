9ea57694b79398243398388462947f90a25e76cc
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-08 14:44:24 -0300

    Update funcao_de_entrada_e_saida.py

--- funcao_de_entrada_e_saida.py
+++ funcao_de_entrada_e_saida.py
@@ -1,1 +1,6 @@
 nome = input("Indentifique-se por gentileza: ")
+sobrenome = input("Qual teu sobrenome? ")
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
