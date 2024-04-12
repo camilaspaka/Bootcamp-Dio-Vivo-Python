3f9e49084883a8e10727c7f9fe211f2353ab0e45
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-12 10:53:39 -0300

    Create Operadores_aritmeticos.py

---
+++ Tipos de Operadores em Python/Operadores_aritmeticos.py
@@ -0,0 +1,17 @@
+print(10 - 5 * 2)
+
+print((10 - 5) * 2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 * 2))
+
+print(10 / 2 * 4)
+
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2)
+print(produto1 - produto2)
+print(produto1 / produto2)
+print(produto1 // produto2)
