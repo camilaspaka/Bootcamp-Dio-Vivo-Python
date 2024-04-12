3fa64760848dbc5e87cd20e1767c95b8cad928fb
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-12 11:28:10 -0300

    operadores

--- Tipos de Operadores em Python/Operadores_de_atribuicao.py
+++ Tipos de Operadores em Python/Operadores_de_atribuicao.py
@@ -2,3 +2,39 @@
 
 saldo = 500
 print(saldo)
+
+#Atribuição com adição
+
+saldo = 500
+saldo += 200
+print(saldo)
+
+#Atribuição com subtração
+
+saldo = 500
+saldo -=100
+print(saldo)
+
+#Atribuição com multiplicação
+saldo = 500
+saldo *= 2
+print(saldo)
+
+#Atribuição com divisão
+saldo = 500
+saldo -=5
+print(saldo)
+
+saldo = 500
+saldo //= 5
+print(saldo)
+
+#Atribuição com módulo
+saldo = 500
+saldo %= 480
+print(saldo)
+
+#Atribuição com exponenciação
+saldo = 80
+saldo **= 2
+print(saldo)
