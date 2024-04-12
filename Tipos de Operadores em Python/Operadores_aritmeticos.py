484ddc640c85b44b9a82673492d4e14ac16fa475
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-12 11:01:54 -0300

    Update Operadores_aritmeticos.py

--- Tipos de Operadores em Python/Operadores_aritmeticos.py
+++ Tipos de Operadores em Python/Operadores_aritmeticos.py
@@ -18,3 +18,8 @@ print(produto1 // produto2)
 print(produto1 * produto2)
 print(produto1 % produto2)
 print(produto1 ** produto2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * (2 - 100) ** 2)
+print(x)
+print(y)
