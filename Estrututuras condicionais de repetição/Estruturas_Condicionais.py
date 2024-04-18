408257639986720590138cba70a429d0d715f00a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-18 14:01:16 -0300

    Update Estruturas_Condicionais.py

--- Estrututuras condicionais de repetição/Estruturas_Condicionais.py
+++ Estrututuras condicionais de repetição/Estruturas_Condicionais.py
@@ -1,9 +1,21 @@
-#Exemplo
+#Exemplo IF
 
 saldo = 2000.0
 saque = float(input("Informe o valor do saque: "))
 
 if saldo >= saque:
     print("Sacado!")
-if saldo <= saque:
+if saldo < saque:
     print("Sem dindin")
+
+#Novo exemplo E
+
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("Sacado!")
+else: 
+    print("Sem dindin")
+
+#
