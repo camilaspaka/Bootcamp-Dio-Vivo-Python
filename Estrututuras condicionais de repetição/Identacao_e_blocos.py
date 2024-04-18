a4cb8d023b77bd35d660fb5681d4d7eeeb2d91e6
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-18 11:35:56 -0300

    Update Identacao_e_blocos.py

--- Estrututuras condicionais de repetição/Identacao_e_blocos.py
+++ Estrututuras condicionais de repetição/Identacao_e_blocos.py
@@ -12,9 +12,5 @@ void sacar(double valor) {
 if (this.saldo >= valor) {
 this.saldo -= valor;}}
 
-def sacar(self, valor : float) None:
-    
-    if self.saldo >= valor:
 
-        self.saldo -= valor
 
