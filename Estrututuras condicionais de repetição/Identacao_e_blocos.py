c8df22de1c663a5217965502941b87d9502e170d
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-18 11:35:08 -0300

    Update Identacao_e_blocos.py

--- Estrututuras condicionais de repetição/Identacao_e_blocos.py
+++ Estrututuras condicionais de repetição/Identacao_e_blocos.py
@@ -1,5 +1,20 @@
+#Exemplo de bloco apenas
+
 def sacar(self, valor : float) None:
 
     if self.saldo >= valor:
 
         self.saldo -= valor
+
+# Segundo Exemplo
+
+void sacar(double valor) {
+if (this.saldo >= valor) {
+this.saldo -= valor;}}
+
+def sacar(self, valor : float) None:
+    
+    if self.saldo >= valor:
+
+        self.saldo -= valor
+
