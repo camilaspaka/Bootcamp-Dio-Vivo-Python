c906fc8b1090e5eae90451aa554eb7bc06585900
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-18 18:19:17 -0300

    Update if_elif_else.py

--- Estrututuras condicionais de repetição/if_elif_else.py
+++ Estrututuras condicionais de repetição/if_elif_else.py
@@ -1,13 +1,15 @@
-conta_normal = 2000000
-conta_universitaria = 50000
+conta_normal = 20000
+conta_universitaria = 5000
 saque = 5000
-saldo = 2000000
-cheque_especial = 50000000000
+saldo = 2000
+cheque_especial = 500
 if conta_normal:
     if saldo >= saque:
         print("Saque realizado com sucesso")
     elif saque <= (saldo + cheque_especial):
         print("Saque realizado com cheque especial")
+    else:
+        print("Saque não autorizado")
 elif conta_universitaria:
     if saldo >= saque:
         print("Saque autorizado")
