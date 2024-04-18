8cc7193b4209d1835746f10e110eab7910a2c4f1
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-18 18:13:02 -0300

    Update Estruturas_Condicionais.py

--- Estrututuras condicionais de repetição/Estruturas_Condicionais.py
+++ Estrututuras condicionais de repetição/Estruturas_Condicionais.py
@@ -39,3 +39,20 @@ if idade >= 18:
     print("Maior de idade")
 else:
     print("Menor de idade")
+
+#Exemplo If/Elif e Else
+conta_normal = 2000000
+conta_universitaria = 50000
+saque = 5000
+saldo = 2000000
+cheque_especial = 50000000000
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com cheque especial")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque autorizado")
+    else:
+        print("Saque não autorizado")
