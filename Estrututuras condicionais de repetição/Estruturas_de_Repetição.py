5bcf9c955cb06178ddded5644570584c44ca5f04
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-19 14:59:13 -0300

    Update Estruturas_de_Repetição.py

--- Estrututuras condicionais de repetição/Estruturas_de_Repetição.py
+++ Estrututuras condicionais de repetição/Estruturas_de_Repetição.py
@@ -9,3 +9,16 @@ print(a)
 a += 1
 
 print(a)
+
+#Com repetição
+
+
+
+#for  
+texto = input("Informe um texto: ")
+Vogais = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in Vogais:
+        print(letra, end="")
+print()
