e118f64e5d3e631723bec25c275bb430a30a3962
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-20 14:47:02 -0300

    Update String_de_multiplas_linhas.py

--- Dominando Strings e Fatiamento/String_de_multiplas_linhas.py
+++ Dominando Strings e Fatiamento/String_de_multiplas_linhas.py
@@ -7,8 +7,11 @@ Olá meu nome é {nome},
 Eu estou aprendendo Python
 """
 
-mensagem = f'''
+mensagem1 = f'''
     Olá meu nome é {nome},
  Eu estou aprendendo Python.
       Essa mensagem tem diferentes recursos.
 '''
+
+print(mensagem)
+print(mensagem1)
