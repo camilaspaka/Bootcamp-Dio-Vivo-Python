8053a21c6343979f4c01dc10bcea5894de2a1af4
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-20 13:43:48 -0300

    Update Interpolação_de_Variaveis.py

--- Dominando Strings e Fatiamento/Interpolação_de_Variaveis.py
+++ Dominando Strings e Fatiamento/Interpolação_de_Variaveis.py
@@ -5,10 +5,14 @@ idade = 29
 profissao = "Dev"
 linguagem = "Python"
 
-print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e  estou matriculada no curso de %s." % (nome, idade, profissao, linguagem))
+print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e estou matriculada no curso de %s." % (nome, idade, profissao, linguagem))
 
 #format 
 
 print("Olá, me chamo {}. Eu tenho {} anos de idade, trabalho como {} e estou matriculada no curso de {}.".format(nome,idade,profissao,linguagem))
 
 print("Olá, me chamo {3}. Eu tenho {2} anos de idade, trabalho como {1} e estou matriculada no curso de {0}.".format(linguagem, profissao, idade, nome))
+
+print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculada no curso de {linguaugem}.".format(nome=nome, idade=idade, profissao=profissao, linguagem=linguagem))
+
+print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculada no curso de {linguaguem}.".format(**pessoa))
