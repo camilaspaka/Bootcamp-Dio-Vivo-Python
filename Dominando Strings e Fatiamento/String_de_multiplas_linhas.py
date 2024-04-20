0263722314803713006d20071b6a9ee720afd20e
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-20 14:45:11 -0300

    huh

--- Dominando Strings e Fatiamento/String_de_multiplas_linhas.py
+++ Dominando Strings e Fatiamento/String_de_multiplas_linhas.py
@@ -6,3 +6,9 @@ mensagem = f"""
 Olá meu nome é {nome},
 Eu estou aprendendo Python
 """
+
+mensagem = f'''
+    Olá meu nome é {nome},
+ Eu estou aprendendo Python.
+      Essa mensagem tem diferentes recursos.
+'''
