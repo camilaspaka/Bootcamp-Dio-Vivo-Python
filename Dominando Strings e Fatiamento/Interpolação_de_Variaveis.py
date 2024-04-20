a481a78a0071b8c5174f3707ebc6b50296d4f2f9
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-20 13:09:44 -0300

    Update Interpolação_de_Variaveis.py

---
+++ Dominando Strings e Fatiamento/Interpolação_de_Variaveis.py
@@ -0,0 +1,8 @@
+#old style %
+
+nome  = "Camila"
+idade = 29
+profissao = "Dev"
+linguagem = "Python"
+
+print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e  estou matriculada no curso de %s." % (nome, idade, profissao, linguagem))
