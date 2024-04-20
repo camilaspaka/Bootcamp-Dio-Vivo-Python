f0f7599d4fba4d94e2adc208c7a7dc0d79a382b8
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-20 12:17:52 -0300

    Update Métodos_string.py

--- Dominando Strings e Fatiamento/Métodos_string.py
+++ Dominando Strings e Fatiamento/Métodos_string.py
@@ -1,5 +1,12 @@
-curso = "pYtHon"
+curso = " pYtHon  "
 
 print(curso.upper())
 print(curso.lower())
 print(curso.title())
+
+#eliminando espaços em branco
+
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+
