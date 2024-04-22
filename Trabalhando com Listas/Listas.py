37162488e0947c1da91f33e886a6f371e05a97bb
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-22 14:16:20 -0300

    Update Listas.py

--- Trabalhando com Listas/Listas.py
+++ Trabalhando com Listas/Listas.py
@@ -22,3 +22,11 @@ print(matriz[0])
 print(matriz[0][0])
 print(matriz[0][-1])
 print(matriz[-1][-1])
+
+lista = ["p", "y", "t", "h", "o", "n"]
+print(lista[2:])
+print(lista[:2])
+print(lista[1:3])
+print(lista[0:3:2])
+print(lista[::])
+print(lista[::-1])
