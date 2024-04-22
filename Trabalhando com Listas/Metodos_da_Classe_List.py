0410294a114f623a1a43be80f83f413bcbd3762a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-22 19:12:15 -0300

    Update Metodos_da_Classe_List.py

--- Trabalhando com Listas/Metodos_da_Classe_List.py
+++ Trabalhando com Listas/Metodos_da_Classe_List.py
@@ -64,17 +64,29 @@ print(linguagens)
 
 #[].sort
 
-linguagens =["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp"]
 print(linguagens.sort())
 
-linguagens =["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp"]
 print(linguagens.sort(reverse=True))
 
-linguagens =["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp"]
 print(linguagens.sort(key=lambda x: len(x)))
 
-linguagens =["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp"]
 print(linguagens.sort(key=lambda x: len(x), reverse=True))
+
+#len
+
+linguagens =["python", "js", "c", "java", "csharp"]
+print(len(linguagens))
+
+#[].sorted
+
+linguagens =["python", "js", "c", "java", "csharp"]
+print(sorted(linguagens, key=lambda x: len(x)))
+
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))
 
 #[].clear
 
