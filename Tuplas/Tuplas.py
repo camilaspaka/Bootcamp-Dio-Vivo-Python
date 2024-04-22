5443b89e5bcc41d3194e502144d8cd9009aa07b2
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-22 19:53:03 -0300

    Update Tuplas.py

--- Tuplas/Tuplas.py
+++ Tuplas/Tuplas.py
@@ -1,8 +1,31 @@
-frutas = ("laranja", "pera", "uva")
-print(frutas)
+frutas = ("laranja", "pera", "uva", "melancia", "manga",)
+print(frutas[-2])
+
 letras = tuple("python")
 print(letras)
+
 numeros = tuple([1, 2, 3, 4])
 print(numeros)
+
 pais = ("Brasil",)
 print(pais)
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 5),
+    (6, 5, "c"),
+)
+
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[0][-2])
+print(matriz[-1][-3])
+
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+print(tupla[2:])
+print(tupla[:2])
+print(tupla[1:3])
+print(tupla[0:3:2])
+print(tupla[::])
+print(tupla[::-1])
