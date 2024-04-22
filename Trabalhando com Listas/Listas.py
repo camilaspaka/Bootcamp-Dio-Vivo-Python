98661bfeb07f93514070e53f59070d7591bfd53b
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-22 13:53:00 -0300

    Update Listas.py

--- Trabalhando com Listas/Listas.py
+++ Trabalhando com Listas/Listas.py
@@ -1,9 +1,14 @@
 frutas = ["laranja", "maca", "uva"]
+print(frutas)
 
-frutas = []
+frutas1 = []
+print(frutas1)
 
 letras = list("python")
+print(letras)
 
 numeros = list(range(10))
+print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2024, 2900, "São Paulo", True]
+print(carro)
