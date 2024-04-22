5e04cfa76b7d65cf2365d81741ca203564959064
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-22 14:08:00 -0300

    Update Listas.py

--- Trabalhando com Listas/Listas.py
+++ Trabalhando com Listas/Listas.py
@@ -12,3 +12,13 @@ print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2024, 2900, "São Paulo", True]
 print(carro)
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[0][-1])
+print(matriz[-1][-1])
