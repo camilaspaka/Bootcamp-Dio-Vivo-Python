d9cac434a1e067df64858e9f5604e730fba746bf
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-25 12:23:33 -0300

    Update Funções7.py

--- Funções/Funções7.py
+++ Funções/Funções7.py
@@ -4,9 +4,16 @@ def somar(a, b):
 def subtrair(a, b):
     return a- b
 
+def test(a, b):
+    return a*2 + b*4
+
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é igual a {a} + {b} = {resultado}")
 
 exibir_resultado(10, 10, somar)
 exibir_resultado(10, 10, subtrair)
+exibir_resultado(10, 10, test)
+
+op = somar
+print(op(1, 23))
