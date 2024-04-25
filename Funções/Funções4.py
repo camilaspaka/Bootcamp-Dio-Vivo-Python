6445ea04dd28411b2a83ba34e30217194dfe8093
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-25 11:33:45 -0300

    Create Funções4.py

---
+++ Funções/Funções4.py
@@ -0,0 +1,7 @@
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+exibir_poema("Zen of Python", "Beatiful is better than ugly.", autor="Tim Peters", ano=1999)
