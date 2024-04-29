40d25eba8dfe96386ae957c360d0b0d60050b06e
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-29 11:31:14 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,15 +1,18 @@
 import re
 
-def validade_numero_telefone(phone_number):
-    pattern = re.complie('[(])[0-9]{2}[)]][\n][0-9]')
+def validate_numero_telefone(phone_number):
+    pattern = r"\(\d{2}\) 9\d{4}-\d{4}"
     if re.match(pattern, phone_number):
-        return f'Numero de telefone valido'
+        return f'Número de telefone valido'
     else:
-        return f'Numero invalido'
+        return f'Número de telefone invalido'
     
 phone_number = input()
-result = validade_numero_telefone(phone_number)
+
+result = validate_numero_telefone(phone_number)
 print(result)
 
 #Deu certo aqui, vejamos no treco da Dio
 # e la deu erro, wonderfull
+#esqueci de copiar e colar o código direito kkkk genio
+#não to sabendo dizer onde é o erro, ahhhhhhhhhhhhhhhh
