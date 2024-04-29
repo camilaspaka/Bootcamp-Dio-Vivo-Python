056032410b44fa93e80c56d1fe81e6d1b2586bb1
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-29 11:36:25 -0300

    Update Desafio2.py

---
+++ Desafio2.py
@@ -0,0 +1,20 @@
+# TODO: Crie uma Lista 'itens' para armazenar os equipamentos:
+Lista_Perifericos["Antena", "Roteador", "Switch"]
+Lista_Produto["Servidor","Cabinet_Rack","Acess_Point"]
+Lista_Cabos["Edge_Routers", "Patch_Cord", "UPS"]
+
+# TODO: Crie um loop para solicita os itens ao usuário:
+
+# TODO: Solicite o item e armazena na variável "item":
+
+# TODO: Adicione o item à lista "itens":
+
+
+# Exibe a lista de itens
+print("Lista de Equipamentos:")  
+for item in itens:
+    # Loop que percorre cada item na lista "itens"
+    print(f"- {item}")
+
+
+#copiei diretamente do curso e é isso, vou arrumando e ta ok
