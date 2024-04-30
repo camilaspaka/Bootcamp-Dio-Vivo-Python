dbf5d172fd2192fb6f6f27e29222290e8daa42a0
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-30 11:48:19 -0300

    Update _init_.py

--- POO/Introdução/_init_.py
+++ POO/Introdução/_init_.py
@@ -1,7 +1,8 @@
 class Cachorro:
     def __init__(self, nome, cor, acordado=True):
+        print("Inicializando a classe...")
         self.nome = nome
         self.cor = cor
         self.acordado = acordado
-        
+
         
