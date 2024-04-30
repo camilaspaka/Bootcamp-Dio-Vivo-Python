60587311008d509a9f0fc94f03ff4fc85557af55
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-30 10:59:37 -0300

    Create Objeto.py

---
+++ POO/Objeto.py
@@ -0,0 +1,8 @@
+cao_1 = Cachorro("pitty", "caramelo com preto", False)
+cao_2 = Cachorro("paçoca", "caramelho com preto")
+
+cao_1.latir()
+
+print(cao_2.acordado)
+cao_2.dormir
+print(cao_2.acordado)
