515e6569a7a608db49cb232879d35676bd836798
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-04-30 11:13:25 -0300

    Update PrimeiroProgramaPOO.py

--- POO/PrimeiroProgramaPOO.py
+++ POO/PrimeiroProgramaPOO.py
@@ -1,19 +1,21 @@
 class Bicicleta:
-    def _init_(self, cor ,modelo ,ano ,valor):
+    def _init_(self, cor,modelo ,ano ,valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-        
+
     def buzinar(self):
         print("Pipipi")
+
     def parar(self):
         print("Parando...")
         print("Bicicleta parada")
+
     def correr(self):
         print("vrumvrum")
 
-b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1 = Bicicleta()
 b1.buzinar()
 b1.correr()
 b1.parar()
