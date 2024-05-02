8099b374604d3209b1950f2d7af9fd193da283e9
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-02 13:05:56 -0300

    Update Herança_Multipla.py

--- POO/Herança/Herança_Multipla.py
+++ POO/Herança/Herança_Multipla.py
@@ -1,11 +1,20 @@
 class Animal:
-    pass
+    def _init_(self, nro_patas):
+        self.nro_patas = nro_patas
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
 
 class Mamifero(Animal):
-    pass
+    def _init_(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super()._init_(**kw)
 
 class Ave(Animal):
-    pass
+    def _init_(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
 
 class Cachorro(Mamifero):
     pass
@@ -17,4 +26,11 @@ class Leao(Mamifero):
     pass
 
 class Ornitorrinco(Mamifero, Ave):
-    pass
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+gato = Gato(nro_patas=4, cor_pelo="Preto")
+print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+print(ornitorrinco)
