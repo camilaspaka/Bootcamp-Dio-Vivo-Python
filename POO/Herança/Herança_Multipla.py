f1ce8c37c25f51758bf868fda93a26ca801d5174
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-02 12:41:00 -0300

    Update Herança_Multipla.py

---
+++ POO/Herança/Herança_Multipla.py
@@ -0,0 +1,20 @@
+class Animal:
+    pass
+
+class Mamifero(Animal):
+    pass
+
+class Ave(Animal):
+    pass
+
+class Cachorro(Mamifero):
+    pass
+
+class Gato(Mamifero):
+    pass
+
+class Leao(Mamifero):
+    pass
+
+class Ornitorrinco(Mamifero, Ave):
+    pass
