12221fd84c293fa5de1ae76b66bc680e1d7e5961
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-04 13:03:41 -0300

    Update Exemplo_Encapsulamento.py

---
+++ POO/Encapsulamento/Exemplo_Encapsulamento.py
@@ -0,0 +1,11 @@
+class Conta:
+    def _init_(self, saldo=0):
+        self.saldo = saldo
+        
+    def depositar(self, valor):
+        pass
+    def sacar(self,valor):
+        pass
+
+conta = Conta(100)
+print(conta._saldo)
