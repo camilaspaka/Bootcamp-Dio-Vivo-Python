d12004cb9d030502793c2a0a62b27d66e0c3dc8f
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-04 13:13:29 -0300

    encapsulamento

--- POO/Encapsulamento/Exemplo_Encapsulamento.py
+++ POO/Encapsulamento/Exemplo_Encapsulamento.py
@@ -1,14 +1,17 @@
 class Conta:
     def _init_(self, nro_agencia , saldo=0):
-        self.saldo = saldo
+        self._saldo = saldo
         self.nro_agencia = nro_agencia
-        
+
     def depositar(self, valor):
         self._saldo += valor
 
     def sacar(self,valor):
         self._saldo -= valor
 
-conta = Conta(100)
+    def mostar_saldo(self):
+        return self._saldo
+
+conta = Conta()
 conta.depositar(100)
 print(conta._saldo)
