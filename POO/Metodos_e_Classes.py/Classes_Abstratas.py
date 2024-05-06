4e93cb4e2161a813f384c1f0566e71ff8c572d9b
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-06 14:49:03 -0300

    Update Classes_Abstratas.py

---
+++ POO/Metodos_e_Classes.py/Classes_Abstratas.py
@@ -0,0 +1,13 @@
+class ControleRemoto:
+    def ligar(self):
+        pass
+
+    def desligar(self):
+        pass
+
+class ControleTV:
+    pass
+
+ControleTV = ControleTV()
+controle.ligar()
+controle.desligar()
