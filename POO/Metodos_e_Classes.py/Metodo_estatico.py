1ad51bd8c27fa3d8879fd81109bef2e299c386af
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-06 13:58:57 -0300

    Update Metodo_estatico.py

--- POO/Metodos_e_Classes.py/Metodo_estatico.py
+++ POO/Metodos_e_Classes.py/Metodo_estatico.py
@@ -3,6 +3,7 @@ class Pessoa:
         self.nome = nome 
         self.idade = idade
     
+    @classmethod
     def criar_apartir_data_nascimento(self, ano, mes, dia, nome):
         idade = 2024 - ano
         return Pessoa(nome, idade)
@@ -10,5 +11,5 @@ class Pessoa:
 p = Pessoa("Camila", 29)
 print(p.nome, p.idade)
 
-p1 = Pessoa().criar_apartir_data_nascimento(1995, 02, 06, "Camila")
-print(p1)
+p1 = Pessoa().criar_apartir_data_nascimento(1995, 2, 6, "Camila")
+print(p1.nome, p1.idade)
