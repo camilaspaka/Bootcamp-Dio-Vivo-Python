3ab0a4376b13729261828b6a7217b598f752294f
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-20 10:17:16 -0300

    Update decoradores.py

--- POO/Decoradores, Iteradores e Geradores/decoradores.py
+++ POO/Decoradores, Iteradores e Geradores/decoradores.py
@@ -6,12 +6,9 @@ def incentivar_aprender(nome):
     print('executando incentivar aprender')
     return f"Oi {nome}, vamos aprender Python! "
 
-def mensagem_para_camila(funcao_mensagem):
-    return funcao_mensagem("Camila")
-
 def executar(funcao):
     print('executando executar')
-    return funcao()
+    return funcao('Cah')
 
-executar(mensagem_para_camila(dizer_oi))
-executar(mensagem_para_camila(incentivar_aprender))
+executar(incentivar_aprender)
+executar(incentivar_aprender)
