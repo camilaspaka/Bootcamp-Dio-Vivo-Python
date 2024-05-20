ae9cb4dab03fa86437d9f1d9e2c86afeed2c3a88
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-20 11:23:51 -0300

    Create kwargs_args.py

---
+++ POO/Decoradores, Iteradores e Geradores/kwargs_args.py
@@ -0,0 +1,14 @@
+def duplicar(func):
+    def envelope(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+
+    return envelope
+
+
+@duplicar
+def aprender(tecnologia):
+    print(f'Estou aprendendo {tecnologia}')
+
+
+aprender('Python')
