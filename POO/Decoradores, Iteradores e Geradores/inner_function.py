02c7fcc023126e89f2303eb2c675878906df0276
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-20 10:04:48 -0300

    Create inner_function.py

---
+++ POO/Decoradores, Iteradores e Geradores/inner_function.py
@@ -0,0 +1,12 @@
+def pai():
+    print("Escrevendo da função pai()")
+
+    def filho1():
+        print("Escrevendo do filho1() função")
+    def filho2():
+        print("Escrevendo do filho2() função")
+
+    filho1()
+    filho2()
+
+pai()
