953fc7892673dfa32fbb3691f843303ba15acd47
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-21 17:53:08 -0300

    poo

--- Teste.py
+++ Teste.py
@@ -1,10 +1,5 @@
-def capitalize_decorator(func): 
-    def wrapper(): 
-        return func().upper() 
-    return wrapper 
-
-@capitalize_decorator 
-def greet(): 
-    return "hello" 
-
-print(greet())
+it = iter([1, 2, 3]) 
+print(next(it)) 
+print(next(it)) 
+print(next(it)) 
+print(next(it))
