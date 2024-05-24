12cf3d93914b6e7b19422d1762a0cb455feeec2a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-24 15:02:38 -0300

    csv

--- POO/Manipulando Arquivos/csv.py
+++ POO/Manipulando Arquivos/csv.py
@@ -1,6 +1,7 @@
 import csv 
 
-with open('example.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+with open('example.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["nome", "idade"])
+    writer.writerow(["Camila", 29])
+    writer.writerow(["Marilia", 31])
