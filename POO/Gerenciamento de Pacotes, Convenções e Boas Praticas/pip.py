a6204bea288a0608e9cd10e9acf433ec1e17fc3a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-24 16:32:09 -0300

    Update pip.py

--- POO/Gerenciamento de Pacotes, Convenções e Boas Praticas/pip.py
+++ POO/Gerenciamento de Pacotes, Convenções e Boas Praticas/pip.py
@@ -2,4 +2,21 @@
 #pip uninstall numpy
 #pip list
 
+
+#intalar um pacote
+#pip install nome_do_pacote
+
+#desinstalar um pacote
+#pip unistall nome_do_pacote
+
+#listar pacotes instalados 
+#pip list
+
+#atualizar pacote
+#pip install --upgrade nome_do_pacote
+
+#procurar por pacotes
+#pip search termo_de_buscar
+
+
 #tudo isso no comando terminal
