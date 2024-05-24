a4e4b0a8a47d9bea8b589049a5fe15d866fb227a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2024-05-24 14:52:57 -0300

    Update boas_praticas.py

--- POO/Manipulando Arquivos/boas_praticas.py
+++ POO/Manipulando Arquivos/boas_praticas.py
@@ -1,5 +1,5 @@
 try:
-    with open('examplee.txt', 'r') as arquivo:
+    with open('examplee.txt', 'r', enconding='utf-8') as arquivo:
          print('trabalhando no arquivo')
 except IOError as exc:
      print("Erro ao abrir o arquivo {exc}")
